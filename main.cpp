9269c8d25055d766d8745646eb22d4d25113e4f3
Alasdair Craig <ac@acraig.za.net>
2017-09-04 15:03:14 +0200

    Added css styling.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,80 @@
 #include "dataaccess.h"
 #include <QApplication>
 
+static const QString styleSheet =
+        "QMainWindow { "
+        "  background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 2, stop: 0 #C0C0C0, stop: 0.8 #FFFFFF); "
+        "}"
+
+        "QTabWidget::pane { "
+        "  border: 2px solid #BB7365; "
+        "  background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 2, stop: 0 #DEADA1, stop: 1 #FFFFFF); "
+        "  border-radius: 4px; "
+        "}"
+
+        "QTabWidget::tab-bar { "
+        "  alignment: center; "
+        "}"
+
+        "QTabBar::tab { "
+        "  border: 2px solid #8F8F91; "
+        "  border-bottom: 0px; "
+        "  border-top-left-radius: 4px; "
+        "  border-top-right-radius: 4px; "
+        "  min-width: 20ex; "
+        "  padding: 6px; "
+        "}"
+
+        "QTabBar::tab:selected { "
+        "  border-color: #BB7365; "
+        "  background: #DEADA1; "
+        "  margin-left: -4px; "
+        "  margin-right: -4px; "
+        "}"
+
+        "QTabBar::tab:!selected { "
+        "  background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #F6F7FA, stop: 1 #DADBDE); "
+        "  margin-top: 2px; "
+        "}"
+
+        "QTabBar::tab:first:selected { "
+        "  margin-left: 0; "
+        "}"
+
+        "QTabBar::tab:last:selected { "
+        "  margin-right: 0; "
+        "}"
+
+        "QTabBar::tab:only-one { "
+        "  margin: 0; "
+        "}"
+
+        "QGroupBox { "
+        "  background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 2, stop: 0 #B3B3D7, stop: 1 #FFFFFF); "
+        "  border: 2px solid #6B78B4; "
+        "  border-radius: 5px; "
+        "  margin-top: 2ex"
+        "}"
+
+        "QLineEdit, QListWidget, QListView { "
+        "  min-height: 20px; "
+        "  border: 1px solid #6B78B4; "
+        "  border-radius: 4px; "
+        "}"
+
+        "QPushButton { "
+        "  border: 2px solid #8F8F91; "
+        "  border-radius: 6px; "
+        "  background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #F6F7FA, stop: 1 #DADBDE); "
+        "  min-width: 80px;"
+        "  min-height: 22px;"
+        "}";
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setStyleSheet(styleSheet);
 
     DataAccess dal;
     dal.OpenOrCreate();
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,6 +18,8 @@ MainWindow::MainWindow(DataAccess &dal, QWidget *parent) :
 
     ui->page1Layout->addWidget(m_editPage);
     ui->page2Layout->addWidget(m_searchPage);
+
+    //m_editPage->setStyleSheet("background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 2, stop: 0 #C09F80, stop: 1 #FFFFFF); ");
 }
 
 MainWindow::~MainWindow()
